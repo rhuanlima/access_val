a7c599001c8ea9d14af9b0773cc1da69cfcb9ad2
Rhuan Lima <rhuan.lima@gmail.com>
2021-01-20 02:41:29 -0300

    Add TabularInLine model

--- acessos/admin.py
+++ acessos/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
 
-from acessos.models import Sistema, Acesso, Area
+from acessos.models import Sistema, Acesso, Area, SystemInstance
 
-admin.site.register(Sistema)
+#admin.site.register(Sistema)
+
+
+@admin.register(SystemInstance)
+class SystemInstanceAdmin(admin.ModelAdmin):
+    pass
+
+
+class SystemInstanceInline(admin.TabularInline):
+    model = SystemInstance
+    list_display = ('dsSistema', 'dsStatus')
+    
+
+
 #admin.site.register(Acesso)
+
 @admin.register(Acesso)
 class AcessoAdmin(admin.ModelAdmin):
     list_display = ('dsMatricula', 'dsUsuario',
@@ -14,10 +28,11 @@ class AcessoAdmin(admin.ModelAdmin):
         ('Usuário', {
             'fields': ('dsMatricula', 'dsUsuario', 'dsArea')
         }),
-        ('Acessos', {
-            'fields': (['dsSistema'])
-        }),
     )
+    inlines = [SystemInstanceInline]
+
 
 
 admin.site.register(Area)
+#admin.site.register(SystemInstance)
+
--- acessos/models.py
+++ acessos/models.py
@@ -1,3 +1,4 @@
+import uuid # Required for unique book instances
 from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 from django.db import models
 
@@ -16,8 +17,8 @@ class Acesso(models.Model):
     dsArea = models.ForeignKey(
         'Area',verbose_name='Area', on_delete=models.SET_NULL, null=True)
 
-    dsSistema = models.ManyToManyField(
-        Sistema, verbose_name='Sistemas', help_text='Selecione os sistemas')
+    # dsSistema = models.ManyToManyField(
+    #     Sistema, verbose_name='Sistemas', help_text='Selecione os sistemas')
     
     dtUpdate = models.DateTimeField(verbose_name = 'Última alteração',auto_now=True)
 
@@ -28,12 +29,27 @@ class Acesso(models.Model):
         return reverse('acessos', args=[str(self.dsMatricula)])
 
 
+class SystemInstance(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    dsSistema = models.ForeignKey(
+        Sistema, verbose_name='Sistemas', help_text='Selecione os sistemas', on_delete=models.SET_NULL, null=True)
+    dsMatricula = models.ForeignKey(
+        Acesso, verbose_name='Matricula', on_delete=models.SET_NULL, null=True)
+    dsStatus = models.BooleanField(
+        'Status', help_text='Deseja manter o acesso ativo?', default=True)
+    class Meta:
+        ordering = ['dsSistema']
+
+    def __str__(self):
+        return f'{self.dsSistema.dsNome} ({self.dsMatricula})'
+
+
 class Area(models.Model):
     """Model representing an author."""
     dsArea = models.CharField('Area',max_length=100)
-    dsCoordenador = models.CharField(max_length=100)
-    dsKeyUser = models.CharField(max_length=100)
-    dsKeyUserEmail = models.CharField(max_length=200)
+    dsCoordenador = models.CharField('Nome do Coordenador', max_length=100)
+    dsKeyUser = models.CharField('Nome do KeyUser', max_length=100)
+    dsKeyUserEmail = models.EmailField(max_length=254)
     class Meta:
         ordering = ['dsArea']
 
