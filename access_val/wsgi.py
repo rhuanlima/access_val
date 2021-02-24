70cfbbe47b2bc5d64c4c1f044904cecee61478f7
Rhuan Lima <rhuan.lima@gmail.com>
2021-02-24 00:34:17 -0300

    add

--- access_val/wsgi.py
+++ access_val/wsgi.py
@@ -7,10 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+from dj_static import Cling
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'access_val.settings')
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
